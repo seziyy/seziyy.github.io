d55389763a6ceeb046c5b6c574e3aa308c4ea81c
seziyy <halesezin@gmail.com>
2026-04-10 13:54:36 +0300

    10.4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
+    unoptimized: true,
     domains: ['i.scdn.co', 'github.com', 'avatars.githubusercontent.com'],
   },
 }
